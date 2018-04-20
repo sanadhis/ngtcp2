1e08508528585c0fe0519097c94ddfe57ed616d4
sanadhis <sanadhis@gmail.com>
2018-04-20 19:32:16 +0200

    --Openssl use manual build

--- ci/gen-certificate.sh
+++ ci/gen-certificate.sh
@@ -5,4 +5,4 @@ mkdir -p cert
 keyfile=cert/server.key
 certfile=cert/server.crt
 
-openssl req -newkey rsa:2048 -x509 -nodes -keyout "$keyfile" -new -out "$certfile" -subj /CN=localhost
+../openssl/build/bin/openssl req -newkey rsa:2048 -x509 -nodes -keyout "$keyfile" -new -out "$certfile" -subj /CN=localhost
